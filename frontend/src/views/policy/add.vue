3736ea8e3364d345bc2b5801466fa8b23237272c
Ewall555 <a03216@foxmail.com>
2024-11-28 09:20:58 +0800

    fix: forntend policy add repeat error

--- frontend/src/views/policy/add.vue
+++ frontend/src/views/policy/add.vue
@@ -1,33 +1,23 @@
 <template>
   <o-form-wrap title="策略管理" @confirm="onConfirm">
-    <el-form
-      ref="ruleForm"
-      class="w-100"
-      :model="formData"
-      :rules="formRules"
-      label-position="top"
-    >
+    <el-form ref="ruleForm" class="w-100" :model="formData" :rules="formRules" label-position="top">
       <el-form-item label="策略名称：" prop="policyname">
         <el-input v-model="formData.policyname" placeholder="请输入策略名称" />
       </el-form-item>
 
       <el-form-item label="最大接待数量：" prop="max_count">
-        <el-input
-          v-model="formData.max_count"
-          type="number"
-          placeholder="请输入最大接待数量"
-        />
+        <el-input-number v-model="formData.max_count" placeholder="请输入最大接待数量" />
       </el-form-item>
 
       <el-form-item label="重复策略：" prop="repeat">
-        <el-input
-          v-model="formData.repeat"
-          type="number"
-          placeholder="请输入重复策略"
-        />
+        <el-select v-model="formData.repeat" placeholder="请选择重复策略">
+          <el-option v-for="option in repeatOptions" :key="option.value" :value="option.value" :label="option.label" />
+        </el-select>
+
       </el-form-item>
 
-      <el-form-item label="工作日：" prop="week">
+      <!-- 只有当 repeat === 1 时，显示 week 字段 -->
+      <el-form-item v-if="formData.repeat === 1" label="工作日：" prop="week">
         <el-input v-model="formData.week" placeholder="请输入工作日标记（如：1111100）" />
       </el-form-item>
 
@@ -58,30 +48,57 @@ const route = useRoute()
 const router = useRouter()
 
 const isEditing = ref(false)
-const uuid = route.params.uuid as string;
+const uuid = route.params.uuid as string
 
-isEditing.value = uuid ? true : false
+isEditing.value = !!uuid
 
 const ruleForm = ref<FormInstance>()
 const formData = reactive<IPolicy>({
-  uuid:'',
+  uuid: '',
   policyname: '',
   max_count: 100,
-  repeat: 1,
+  repeat: 1, // 默认值
   week: '',
   work_times: [{ start_time: '', end_time: '' }]
 })
 
+interface RepeatOption {
+  value: number
+  label: string
+}
+
+const repeatOptions: RepeatOption[] = [
+  { value: 1, label: '自定义[周一至周天]' },
+  { value: 2, label: '每天' },
+  { value: 3, label: '周一至周五' },
+  { value: 4, label: '法定工作日（跳过法定节假日）' },
+  { value: 5, label: '法定节假日（跳过法定工作日）' }
+]
+
+
 const formRules = reactive<FormRules>({
   policyname: [{ required: true, message: '请输入策略名称', trigger: 'blur' }],
   max_count: [{ required: true, message: '请输入最大接待数量', trigger: 'blur' }],
-  repeat: [{ required: true, message: '请输入重复策略', trigger: 'blur' }],
-  week: [{ required: true, message: '请输入工作日标记', trigger: 'blur' }],
+  repeat: [{ required: true, message: '请选择重复策略', trigger: 'change' }],
+  week: [
+    {
+      required: true,
+      message: '当重复策略为自定义时，工作日必填',
+      trigger: 'blur',
+      validator: (rule, value, callback) => {
+        if (formData.repeat === 1 && !/^[01]{7}$/.test(value)) {
+          callback(new Error('请输入有效的工作日标记（如：1111100）'))
+        } else {
+          callback()
+        }
+      }
+    }
+  ],
   work_times: [{ required: true, type: 'array', min: 1, message: '请添加至少一个工作时间', trigger: 'blur' }]
 })
 
 const policyInfo = async () => {
-  const { data } = await getPolicyApi(uuid);
+  const { data } = await getPolicyApi(uuid)
   Object.assign(formData, data)
 }
 
@@ -107,26 +124,20 @@ const onConfirm = (loading: TLoading) => {
 
 const addWorkTime = () => {
   if (Array.isArray(formData.work_times)) {
-    formData.work_times.push({ start_time: '', end_time: '' });
+    formData.work_times.push({ start_time: '', end_time: '' })
   }
 }
 
 const removeWorkTime = async (index: number) => {
   try {
-    await ElMessageBox.confirm(
-      '确定要删除该工作时间吗？',
-      '删除确认',
-      {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }
-    )
-    // 用户点击确认后执行删除操作
+    await ElMessageBox.confirm('确定要删除该工作时间吗？', '删除确认', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      type: 'warning'
+    })
     formData.work_times?.splice(index, 1)
     ElMessage.success('工作时间已删除')
-  } catch {
-  }
+  } catch { }
 }
 
 onBeforeMount(() => {
